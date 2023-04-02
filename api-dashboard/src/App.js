867a8de6c967f9499e12d0866086c49c05036da0
Lê Quang Tùng <20110746@student.hcmute.edu.vn>
2023-04-03 01:29:51 +0700

    fix Test

---
+++ api-dashboard/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./assets/css/style.css";
+import AdminPage from "./pages/admin";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import PageNotFound from "./pages/404";
+
+function App(props) {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/home/*" element={<AdminPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route
+                    path="/"
+                    element={<Navigate replace to="/home/dashboard" />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
--- api-dashboard/src/index.js
+++ api-dashboard/src/index.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import './assets/css/style.css';
 import reportWebVitals from "./reportWebVitals";
-import AdminPage from "./pages/admin";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
-import PageNotFound from "./pages/404"
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-        <Route path="*" element={<PageNotFound/>}/>
-        <Route path="/home/*" element={<AdminPage/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/" element={<Navigate replace to="/home/dashboard" />} />
-    </Routes>
-  </BrowserRouter>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
