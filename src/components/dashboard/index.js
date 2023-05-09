b8a512afa44c4bde52da62f5d241313fffe8067d
Lê Quang Tùng <20110746@student.hcmute.edu.vn>
2023-05-10 00:43:25 +0700

    pieChart

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -6,51 +6,28 @@ import { debounce } from "lodash";
 import axios from "axios";
 
 function Dashboard(props) {
-    const [ok_records, setOk_records] = useState(0);
-    const [fail_records, setFail_records] = useState(0);
-    const [ok_angles, setOk_angles] = useState([]);
-    const [fail_angles, setFail_angles] = useState([]);
-    const [predict_result_statistic, setPredict_result_statistic] = useState(
-        []
-    );
+    const [lastMonthData, setLastMonthData] = useState({
+        "pieChart":[],
+        "barChart":[],
+        "predict":[]
+    });
 
-    // fetch option datas => then fetch 1st record
     useEffect(() => {
         const fetchData = debounce(async function () {
-            const result = await axios.get(
-                "/api/overview",
-                {
-                    params: {
-                        dayStart: 1,
-                        dayEnd: 31,
-                    },
-                }
-            );
-            setOk_records(result.data["ok_records"]);
-            setFail_records(result.data["fail_records"]);
-            setOk_angles(result.data["ok_angles"]);
-            setFail_angles(result.data["fail_angles"]);
-            setPredict_result_statistic(
-                result.data["predict_result_statistic"]
-            );
+            const result = await axios.get("/api/overview");
+            const last_month = result.data.last_month
+            setLastMonthData({
+                "pieChart":[{"name": "ok", "value": last_month.ok.length}, {"name": "fail", "value": last_month.fail.length}],
+                "barChart":[],
+                "predict":[]
+            })
+
+
+            const current_month = result.data.current_month
         });
         fetchData();
     }, []);
 
-    if (
-        !(
-            ok_records &&
-            fail_records &&
-            ok_angles &&
-            fail_angles &&
-            predict_result_statistic
-        )
-    ) {
-        return (
-            // Render a loading indicator while waiting for the data to load
-            <h1>Loading...</h1>
-        );
-    }
     return (
         <div id="dashboard">
             <Container className="lastest-report">
@@ -60,10 +37,7 @@ function Dashboard(props) {
                         <Card className="mb-3 pb-3 container d-flex center">
                             <h5 className="p-3">Status percentage</h5>
                             <PieChartComponent
-                                data={[
-                                    { name: "ok", value: ok_records },
-                                    { name: "fail", value: fail_records },
-                                ]}
+                                data={lastMonthData.pieChart}
                                 width={300}
                                 height={300}
                                 label={true}
@@ -73,9 +47,50 @@ function Dashboard(props) {
                     </Col>
                     <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
+                            <h5 className="p-3">Status percentage</h5>
+                            <PieChartComponent
+                                data={[]}
+                                width={300}
+                                height={300}
+                                label={true}
+                                legend={true}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="report-content">
+                <Col md="12" lg="6">
+                        <Card className="mb-3 pb-3 container d-flex center">
                             <h5 className="p-3 ms-5">Ok angles</h5>
                             <BarChartComponent
-                                data={ok_angles.map((item) => {
+                                data={[
+                                    [1, 1],
+                                    [1, 2],
+                                    [1, 3],
+                                ].map((item) => {
+                                    return {
+                                        name: item[0],
+                                        "Angles: ok": item[1],
+                                    };
+                                })}
+                                width={500}
+                                height={300}
+                                axis={true}
+                                legend={true}
+                                color="#ade7b6"
+                                name="Angles: ok"
+                            />
+                        </Card>
+                    </Col>
+                    <Col md="12" lg="6">
+                        <Card className="mb-3 pb-3 container d-flex center">
+                            <h5 className="p-3 ms-5">Ok angles</h5>
+                            <BarChartComponent
+                                data={[
+                                    [1, 1],
+                                    [1, 2],
+                                    [1, 3],
+                                ].map((item) => {
                                     return {
                                         name: item[0],
                                         "Angles: ok": item[1],
@@ -98,7 +113,11 @@ function Dashboard(props) {
                                 Predict result statistic
                             </h5>
                             <BarChartComponent
-                                data={predict_result_statistic.map((item) => {
+                                data={[
+                                    [1, 1],
+                                    [1, 2],
+                                    [1, 3],
+                                ].map((item) => {
                                     return {
                                         name: item[0],
                                         "Predict result": item[1],
@@ -114,20 +133,25 @@ function Dashboard(props) {
                     </Col>
                     <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
-                            <h5 className="p-3 ms-5">Fail Angles</h5>
+                            <h5 className="p-3 ms-5">
+                                Predict result statistic
+                            </h5>
                             <BarChartComponent
-                                data={fail_angles.map((item) => {
+                                data={[
+                                    [1, 1],
+                                    [1, 2],
+                                    [1, 3],
+                                ].map((item) => {
                                     return {
                                         name: item[0],
-                                        "Angles: fail": item[1],
+                                        "Predict result": item[1],
                                     };
                                 })}
                                 width={500}
                                 height={300}
                                 axis={true}
                                 legend={true}
-                                color="#ffa69e"
-                                name="Angles: fail"
+                                name="Predict result"
                             />
                         </Card>
                     </Col>
