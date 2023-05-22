0140b1c1fbff26e0fd0da121f80a4fc434fcdcb6
Lê Quang Tùng <20110746@student.hcmute.edu.vn>
2023-05-22 19:05:16 +0700

    up pie-charts

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -23,89 +23,100 @@ function Dashboard(props) {
         const fetchData = debounce(async function () {
             const result = await axios.get("/api/overview");
             const last_month = result.data.last_month;
-            const barChart_lastmonth = []
+            const pieChart_lastmonth = [
+                { name: "ok", value: 0 },
+                { name: "fail", value: 0 },
+            ];
+
+            const barChart_lastmonth = [];
             for (let i = 1; i <= 7; i++) {
                 barChart_lastmonth.push({
-                    "name": i,
-                    "ok": 0,
-                    "fail": 0,
+                    name: i,
+                    ok: 0,
+                    fail: 0,
                     //"statistic_predict_results": 0
-                })
+                });
             }
-
-            //BarChart of statistic_predict_results
-            const barChart2_lastmonth = []
-
-            let value_tmp = 0;
-            for (let i = 1; i < last_month.statistic_predict_results.length; i++) {
-                if(i % 10 === 0)
-                {
-                    barChart2_lastmonth.push({
-                        "name": i/10,
-                        "statistic_predict_results": value_tmp
-                    })
-                    value_tmp = 0;
-                }
-                else{
-                    value_tmp += last_month.statistic_predict_results[i-1][1];
-                }
-            }
-
-
-            last_month.ok.forEach(ok=>barChart_lastmonth[ok[0]-1].ok += ok[1])
-            last_month.fail.forEach(fail=>barChart_lastmonth[fail[0]-1].fail += fail[1])
-           
-
-            setLastMonthData({
-                pieChart: [
-                    { "name": "ok", "value": last_month.ok.length },
-                    { "name": "fail", "value": last_month.fail.length },
-                    
-                ],
-                barChart: barChart_lastmonth,
-                predict: barChart2_lastmonth
+            last_month.ok.forEach((ok) => {
+                barChart_lastmonth[ok[0] - 1].ok += ok[1];
+                pieChart_lastmonth[0].value += ok[1];
+            });
+            last_month.fail.forEach((fail) => {
+                barChart_lastmonth[fail[0] - 1].fail += fail[1];
+                pieChart_lastmonth[1].value += fail[1];
             });
 
+            //BarChart of statistic_predict_results
+            const barChart2_lastmonth = [];
+
+            let value_tmp = 0;
+            for (
+                let i = 1;
+                i < last_month.statistic_predict_results.length;
+                i++
+            ) {
+                if (i % 10 === 0) {
+                    barChart2_lastmonth.push({
+                        name: i / 10,
+                        statistic_predict_results: value_tmp,
+                    });
+                    value_tmp = 0;
+                } else {
+                    value_tmp += last_month.statistic_predict_results[i - 1][1];
+                }
+            }
+
+            setLastMonthData({
+                pieChart: pieChart_lastmonth,
+                barChart: barChart_lastmonth,
+                predict: barChart2_lastmonth,
+            });
 
             //Current Month Fragment
             const current_month = result.data.current_month;
-            const barChart_currentmonth = []
+            const pieChart_currentmonth = [
+                { name: "ok", value: 0 },
+                { name: "fail", value: 0 },
+            ];
+            const barChart_currentmonth = [];
             for (let i = 1; i <= 7; i++) {
                 barChart_currentmonth.push({
-                    "name": i,
-                    "ok": 0,
-                    "fail": 0
-                })
+                    name: i,
+                    ok: 0,
+                    fail: 0,
+                });
             }
-
+            current_month.ok.forEach((ok) => {
+                barChart_currentmonth[ok[0] - 1].ok += ok[1];
+                pieChart_currentmonth[0].value += ok[1];
+            });
+            current_month.fail.forEach((fail) => {
+                barChart_currentmonth[fail[0] - 1].fail += fail[1];
+                pieChart_currentmonth[1].value += fail[1];
+            });
 
             //BarChart of statistic_predict_results
-            const barChart2_currentmonth = []
+            const barChart2_currentmonth = [];
 
             let value_tmp2 = 0;
-            for (let i = 1; i < current_month.statistic_predict_results.length; i++) {
-                if(i % 10 === 0)
-                {
+            for (
+                let i = 1;
+                i < current_month.statistic_predict_results.length;
+                i++
+            ) {
+                if (i % 10 === 0) {
                     barChart2_currentmonth.push({
-                        "name": i/10,
-                        "statistic_predict_results": value_tmp2
-                    })
+                        name: i / 10,
+                        statistic_predict_results: value_tmp2,
+                    });
                     value_tmp2 = 0;
-                }
-                else{
-                    value_tmp2 += current_month.statistic_predict_results[i-1][1];
-                    
+                } else {
+                    value_tmp2 +=
+                        current_month.statistic_predict_results[i - 1][1];
                 }
             }
-            
-
-            current_month.ok.forEach(ok=>barChart_currentmonth[ok[0]-1].ok += ok[1])
-            current_month.fail.forEach(fail=>barChart_currentmonth[fail[0]-1].fail += fail[1])
             setCurrentMonthData({
-                pieChart: [
-                    { "name": "ok", "value": current_month.ok.length },
-                    { "name": "fail", "value": current_month.fail.length },
-                ],
+                pieChart: pieChart_currentmonth,
                 barChart: barChart_currentmonth,
                 predict: barChart2_currentmonth,
             });
@@ -119,8 +130,20 @@ function Dashboard(props) {
                 <h3 className="report-header pb-4">Overview</h3>
                 <Row className="report-content">
                     <Col md="12" lg="6">
+                        <h5 className="report-header pb-4 text-center">
+                            Last month report
+                        </h5>
+                    </Col>
+                    <Col md="12" lg="6">
+                        <h5 className="report-header pb-4 text-center">
+                            Current month report
+                        </h5>
+                    </Col>
+                </Row>
+                <Row className="report-content">
+                    <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
-                            <h5 className="p-3">Status percentage</h5>
+                            <h5 className="p-3">Ok / Fail percentage</h5>
                             <PieChartComponent
                                 data={lastMonthData.pieChart}
                                 width={300}
@@ -132,7 +155,7 @@ function Dashboard(props) {
                     </Col>
                     <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
-                            <h5 className="p-3">Status percentage</h5>
+                            <h5 className="p-3">Ok / Fail percentage</h5>
                             <PieChartComponent
                                 data={currentMonthData.pieChart}
                                 width={300}
@@ -146,7 +169,7 @@ function Dashboard(props) {
                 <Row className="report-content">
                     <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
-                            <h5 className="p-3 ms-5">Ok angles</h5>
+                            <h5 className="p-3 ms-5">Ok / Fail by angles</h5>
                             <BarChart2ColComponent
                                 data={lastMonthData.barChart}
                                 width={500}
@@ -158,7 +181,7 @@ function Dashboard(props) {
                     </Col>
                     <Col md="12" lg="6">
                         <Card className="mb-3 pb-3 container d-flex center">
-                            <h5 className="p-3 ms-5">Ok angles</h5>
+                            <h5 className="p-3 ms-5">Ok / Fail by angles</h5>
                             <BarChart2ColComponent
                                 data={currentMonthData.barChart}
                                 width={500}
